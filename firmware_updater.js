e8d826006a7f370c98fc78497882178f75ab11aa
Rory Olsen <roris.solaris@gmail.com>
2020-04-20 16:07:43 -0600

    First commit.

---
+++ firmware_updater.js
@@ -0,0 +1,139 @@
+const { program } = require('commander')
+const q = require('q')
+const device_curator = require('ljswitchboard-ljm_device_curator')
+const labjack_nodejs = require('labjack-nodejs')
+const ljm = labjack_nodejs.driver()
+
+// The globally-scoped instance
+let updateInst
+
+class DeviceUpdater {
+    constructor(opts) {
+        this.dt = opts.deviceType
+        this.ct = opts.connectionType
+        this.id = opts.identifier
+        this.fwFile = opts.file
+        this.dryRun = opts.dryRun
+
+        this.device = new device_curator.device()
+        this.debug = false
+    }
+
+    destroy() {
+        this.device.destroy()
+    }
+
+    displayErrorAndQuit(err) {
+        console.log('displayErrorAndQuit')
+        if (this.debug === true) {
+            console.error(err)
+        }
+        else {
+            console.error(err.message)
+        }
+    }
+
+    openDevice() {
+        let deferred = q.defer()
+
+        console.log(
+            "Open parameters :",
+            this.dt,
+            this.ct,
+            this.id
+        )
+
+        this.device.open(this.dt, this.ct, this.id)
+        .then((res) => {
+            console.log(
+                "Opened :",
+                res.productType,
+                res.connectionTypeName,
+                res.serialNumber
+            )
+            deferred.resolve(res)
+        }, (err) => {
+            deferred.reject(new Error(`Failed for testing: ${ljm.errToStrSync(err)}`))
+        })
+        return deferred.promise
+    }
+
+    updateFirmware() {
+        if (this.dryRun) {
+            console.log('Dry run: Skipping firmware update.')
+            return
+        }
+
+        console.log('Now updating to firmware file:', this.fwFile)
+        var percentListener = (/*value*/) => {
+            var deferred = q.defer()
+            // console.log("", value.toString() + '%')
+            deferred.resolve()
+            return deferred.promise
+        }
+        var stepListener = (value) => {
+            var deferred = q.defer()
+            console.log("" + value.toString())
+            deferred.resolve()
+            return deferred.promise
+        }
+
+        return this.device.updateFirmware(this.fwFile, percentListener, stepListener)
+    }
+
+    printFWInfo() {
+        return this.device.iRead('FIRMWARE_VERSION')
+        .then((fwVer) => {
+            console.log(`Current firmware version: ${fwVer.val}`)
+        })
+    }
+
+    closeDevice() {
+        return this.device.close()
+    }
+}
+
+function main() {
+    program
+        .option(
+            '-d, --device-type <deviceType>',
+            'The LabJack device type. E.g.: T4, T7, ANY',
+            'ANY',
+        )
+        .option(
+            '-c, --connection-type <connectionType>',
+            'The connection type to be used to connect to the device. E.g.: USB, ETHERNET, WIFI, ANY',
+            'ANY',
+        )
+        .requiredOption(
+            '-i, --identifier <identifier>',
+            '(Required) A device\'s serial number, IP address, name, or ANY. E.g.: 470010835, 192.168.1.207, My-HVAC-Lab-DAQ, ANY',
+        )
+        .requiredOption(
+            '-f, --file <file>',
+            '(Required) The LabJack firmware file path. E.g.: /home/me/Downloads/T7firmware_010292_2020-02-04_0.bin, C:\\Users\\me\\Downloads\\T7firmware_010292_2020-02-04_0.bin',
+        )
+        // .option(
+        //     '-q, --quiet',
+        //     'Avoid producing any stdout output. Still produces stderr output.',
+        //     false,
+        // )
+        .option(
+            '-n, --dry-run',
+            'Open a device and read the firmware version without updating firmware. File parameter is still required but will not be read.',
+            false,
+        )
+
+    program.parse()
+
+    updateInst = new DeviceUpdater(program.opts())
+    updateInst.openDevice()
+    .then(() => { return updateInst.printFWInfo() })
+    .then(() => { return updateInst.updateFirmware() })
+    .then(() => { return updateInst.printFWInfo() })
+    .then(() => { return updateInst.closeDevice() })
+    .catch((err) => { return updateInst.displayErrorAndQuit(err) })
+    .finally(() => { return updateInst.destroy() })
+}
+
+main()
---
+++ readme.md
@@ -0,0 +1,65 @@
+# firmware_updater_maker
+
+Creates a firmware updater (command-line executable) for T-series LabJack devices. Cross-platform.
+
+Uses `ljswitchboard-ljm_device_curator` and `pkg`.
+
+
+## Requirements
+
+Requires ES6. Written / tested with Node v10.2.1.
+
+Run on a 64-bit system.
+
+
+## Setup
+
+```
+cd firmware_updater_maker
+npm install
+npm install -g pkg
+```
+
+
+## Create firmware updater
+
+```
+cd firmware_updater_maker
+mkdir output/
+pkg --target node10-linux-x64   ./firmware_updater.js --output ./output/labjack-firmware-updater-node10-linux-x64
+pkg --target node10-macos-x64   ./firmware_updater.js --output ./output/labjack-firmware-updater-node10-macos-x64
+pkg --target node10-win-x64     ./firmware_updater.js --output ./output/labjack-firmware-updater-node10-win-x64
+```
+
+For a given platform, pkg [apparently needs both the native node binary and the target node binary](https://github.com/zeit/pkg/issues/869). There's [no 32-bit or arm node binaries for macOS](https://github.com/zeit/pkg-fetch/releases/), so I had to run the following on macOS, which [creates an executable that includes the script source](https://github.com/zeit/pkg-fetch/issues/68#issuecomment-511656541):
+
+```
+pkg --target node10-linux-x86   ./firmware_updater.js --output ./output/labjack-firmware-updater-node10-linux-x86 --no-bytecode --public --public-packages '*'
+pkg --target node10-linux-armv7 ./firmware_updater.js --output ./output/labjack-firmware-updater-node10-linux-armv7 --no-bytecode --public --public-packages '*'
+```
+
+
+## Firmware updater usage
+
+`node firmware_updater.js` usage is the same as for the resulting binaries in `output/`.
+
+
+### Examples:
+
+For usage:
+
+```
+./device-updater.out --help
+```
+
+Which is the same as:
+
+```
+node ./firmware_updater.js
+```
+
+Example updating the 470010835 T7 via USB:
+
+```
+node firmware_updater.js -i 470010835 -f ~/Downloads/T7firmware_010292_2020-02-04_0.bin -d t7 -c USB
+```
